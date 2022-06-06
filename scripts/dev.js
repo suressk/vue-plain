65eb853eec18393f196b3dc3c2b51d07dd0b8bd6
suressk <sure_ssk@outlook.com>
2022-06-07 04:45:31 +0800

    feat: add eslint and prettier file

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,6 +1,6 @@
 const { build } = require('esbuild')
 const { resolve, relative } = require('path')
-// 打包命令参数 node scripts/dev.js reactivity -f global
+// 打包命令参数: node scripts/dev.js reactivity -f global
 // 得到 { _: ['reactivity'], f: 'global' }
 const args = require('minimist')(process.argv.slice(2))
 
@@ -13,33 +13,31 @@ const format = args.f || 'global'
 const pkg = require(resolve(__dirname, `../packages/${target}/package.json`))
 
 // 打包输出格式
-const outputFormat = format === 'global'
-    ? 'iife'
-    : format === 'cjs'
-    ? 'cjs'
-    : 'esm'
+const outputFormat =
+  format === 'global' ? 'iife' : format === 'cjs' ? 'cjs' : 'esm'
 
 // 输出文件路径及文件名
 const outfile = resolve(
-    __dirname,
-    `../packages/${target}/dist/${target}.${format}.js`
+  __dirname,
+  `../packages/${target}/dist/${target}.${format}.js`
 )
 
+// 相对 root 的打包结果路径
 const relativeOutfile = relative(process.cwd(), outfile)
 
 build({
-    entryPoints: [resolve(__dirname, `../packages/${target}/src/index.ts`)],
-    outfile,
-    bundle: true,
-    sourcemap: true,
-    format: outputFormat,
-    globalName: pkg.buildOptions?.name,
-    platform: format === 'cjs' ? 'node' : 'browser',
-    watch: {
-      onRebuild(error) {
-        if (!error) console.log(`rebuilt: ${relativeOutfile}`)
-      }
+  entryPoints: [resolve(__dirname, `../packages/${target}/src/index.ts`)],
+  outfile,
+  bundle: true,
+  sourcemap: true,
+  format: outputFormat,
+  globalName: pkg.buildOptions?.name,
+  platform: format === 'cjs' ? 'node' : 'browser',
+  watch: {
+    onRebuild(error) {
+      if (!error) console.log(`rebuilt: ${relativeOutfile}`)
     }
-  }).then(() => {
-    console.log(`watching: ${relativeOutfile}`)
-  })
+  }
+}).then(() => {
+  console.log(`watching: ${relativeOutfile}`)
+})
